7918b34853fbdad279099ec3f9e05ba8ac0d46e0
personalmac <aalleexsh2011@yandex.ru>
2022-10-19 20:24:53 +0300

    1.2: course information, step2

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -11,35 +11,39 @@ const App = () => {
   const Header = (props) => {
 
     return (
-      <>
+      <div>
         <h1>{props.course}</h1>
-      </>
+      </div>
+    )
+  }
+  const Part = (props) => {
+
+    return (
+      <div>
+        <p>
+          {props.part} {props.exercises}
+        </p>
+      </div>
     )
   }
   const Content = (props) => {
 
     return (
-      <>
-        <p>
-          {props.part1} {props.exercises1}
-        </p>
-        <p>
-          {props.part2} {props.exercises2}
-        </p>
-        <p>
-          {props.part3} {props.exercises3}
-        </p>
-      </>
+      <div>
+        <Part part={props.part1} exercises={props.exercises1}/>
+        <Part part={props.part2} exercises={props.exercises2}/>
+        <Part part={props.part3} exercises={props.exercises3}/>
+      </div>
     )
   }
   const Total = (props) => {
 
     return (
-      <>
+      <div>
         <p>
           Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}
         </p>
-      </>
+      </div>
     )
   }
   
