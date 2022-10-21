877a4997abf13748cb7588256816193c172a945a
personalmac <aalleexsh2011@yandex.ru>
2022-10-22 01:48:42 +0300

    2.9*: The Phonebook Step4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,5 +1,18 @@
 import { useState } from "react";
 
+const Filter = ({ onFilterChange }) => {
+  const [newFilter, setNewFilter] = useState("");
+  const handleInputChange = (event) => {
+    setNewFilter(event.target.value);
+    onFilterChange(event.target.value);
+  };
+  return (
+    <div>
+      filter shown with <input value={newFilter} onChange={handleInputChange} />
+    </div>
+  );
+};
+
 const Person = ({ person }) => {
   return (
     <div>
@@ -10,10 +23,14 @@ const Person = ({ person }) => {
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-1234567", id: 1 },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [newFilter, setNewFilter] = useState("");
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -47,10 +64,19 @@ const App = () => {
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value);
   };
+  const handleFilterChange = (value) => {
+    setNewFilter(value);
+  };
+
+  const personsToShow = persons.filter((person) =>
+    person.name.toUpperCase().includes(newFilter.toUpperCase())
+  );
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <Filter onFilterChange={handleFilterChange} />
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -63,7 +89,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
+      {personsToShow.map((person) => (
         <Person key={person.id} person={person} />
       ))}
     </div>
