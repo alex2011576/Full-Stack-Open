b37015d1d4c46635ad2531be3d52f98655860ff6
personalmac <aalleexsh2011@yandex.ru>
2022-10-27 19:41:21 +0300

    3.22: Lint configuration

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,113 +1,114 @@
-const express = require("express");
-const morgan = require("morgan");
-const app = express();
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
 
-morgan.token("bodyPOST", function (req, res) {
-  if (req.method === "POST") {
-    return JSON.stringify(req.body);
+// eslint-disable-next-line no-unused-vars
+morgan.token('bodyPOST', function (req, res) {
+  if (req.method === 'POST') {
+    return JSON.stringify(req.body)
   }
-});
+})
 
 app.use(
   morgan(
-    ":method :url :status :res[content-length] - :response-time ms :bodyPOST"
+    ':method :url :status :res[content-length] - :response-time ms :bodyPOST'
   )
-);
-app.use(express.json());
+)
+app.use(express.json())
 
 let persons = [
   {
     id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
+    name: 'Arto Hellas',
+    number: '040-123456',
   },
   {
     id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
+    name: 'Ada Lovelace',
+    number: '39-44-5323523',
   },
   {
     id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
+    name: 'Dan Abramov',
+    number: '12-43-234345',
   },
   {
     id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122',
   },
-];
+]
 
 // app.get("/", (request, response) => {
 //   response.send("<h1>Welcome to Phonebook!</h1>");
 // });
 
-app.get("/api/persons", (request, response) => {
-  response.json(persons);
-});
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
 
-app.get("/info", (request, response) => {
-  const time = new Date();
-  const count = persons.length;
+app.get('/info', (request, response) => {
+  const time = new Date()
+  const count = persons.length
   response.send(
     `<p>Phonebook has info for ${count} people</p>
       <p>${time}</p>`
-  );
-});
+  )
+})
 
-app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((person) => person.id === id);
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find((person) => person.id === id)
 
   if (person) {
-    response.json(person);
+    response.json(person)
   } else {
-    response.status(404).send("Error 404: non-existent person");
+    response.status(404).send('Error 404: non-existent person')
   }
-});
+})
 
-app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter((person) => person.id !== id);
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter((person) => person.id !== id)
 
-  response.status(204).end();
-});
+  response.status(204).end()
+})
 
 const generateId = (persons) => {
-  const randomId = () => Math.floor(Math.random() * Number.MAX_VALUE);
-  let newId = randomId();
+  const randomId = () => Math.floor(Math.random() * Number.MAX_VALUE)
+  let newId = randomId()
   for (let i = 0; persons.find((p) => p.id === randomId); i++) {
-    newId = randomId();
+    newId = randomId()
   }
-  return newId;
-};
+  return newId
+}
 
-app.post("/api/persons", (request, response) => {
-  const body = request.body;
+app.post('/api/persons', (request, response) => {
+  const body = request.body
 
   if (!body.name || !body.number) {
     return response.status(400).json({
-      error: "Entry should contain both name and number",
-    });
+      error: 'Entry should contain both name and number',
+    })
   }
   if (persons.find((p) => p.name === body.name)) {
     return response.status(400).json({
-      error: "name must be unique",
-    });
+      error: 'name must be unique',
+    })
   }
 
   const person = {
     id: generateId(persons),
     name: body.name,
     number: body.number,
-  };
+  }
 
-  persons = persons.concat(person);
+  persons = persons.concat(person)
 
-  response.json(person);
-});
+  response.json(person)
+})
 
-const PORT = 3001;
+const PORT = 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
