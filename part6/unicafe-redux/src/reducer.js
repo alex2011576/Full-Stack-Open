cfb0f6a4f41e24105f60dc847a5cc936423ec324
personalmac <aalleexsh2011@yandex.ru>
2022-10-31 13:43:28 +0200

    6.1: unicafe revisited, step1

---
+++ part6/unicafe-redux/src/reducer.js
@@ -0,0 +1,39 @@
+const initialState = {
+  good: 0,
+  ok: 0,
+  bad: 0,
+};
+
+const counterReducer = (state = initialState, action) => {
+  // eslint-disable-next-line no-console
+  console.log(action);
+  switch (action.type) {
+    case 'GOOD': {
+      const newState = {
+        ...state,
+        good: state.good + 1,
+      };
+      return newState;
+    }
+    case 'OK': {
+      const newState = {
+        ...state,
+        ok: state.ok + 1,
+      };
+      return newState;
+    }
+    case 'BAD': {
+      const newState = {
+        ...state,
+        bad: state.bad + 1,
+      };
+      return newState;
+    }
+
+    case 'ZERO':
+      return initialState;
+    default: return state;
+  }
+};
+
+export default counterReducer;
